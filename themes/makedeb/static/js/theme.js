0c46ab3571bb63ea19883d93362a1d3ae186aed0
suman <ronaldosuman97@gmail.com>
2023-02-19 09:16:46 +0530

    Theme script seperated

---
+++ themes/makedeb/static/js/theme.js
@@ -0,0 +1,58 @@
+initialThemeLoad()
+
+	const toggleThemeBtn = document.getElementById("toggle-theme-button")
+	toggleThemeBtn?.addEventListener("click",function(){
+		const body = document.body
+		if (body.className=="dark")
+		{
+			setBodyClass("")
+			toggleThemeBtn?.classList.remove("active")
+			changeBtnIcon("moon")
+			localStorage.removeItem("theme")
+		}
+		else {
+			toggleThemeBtn?.classList.add("active")
+			changeBtnIcon("sun")
+			localStorage.setItem("theme", "dark")
+			setBodyClass("dark")
+		}
+	})
+
+	/**
+	 * Makes nessesary changes for initial theme load
+	*/
+	function initialThemeLoad(){
+	const toggleThemeBtn = document.getElementById("toggle-theme-button")
+	const theme = localStorage.getItem("theme")
+		if (theme == "dark") {
+			setBodyClass("dark")
+			changeBtnIcon("sun")
+			toggleThemeBtn?.classList.add("active")
+		} else changeBtnIcon("moon")
+	}
+
+	/**
+	 * Changes the body class
+	 * @param {string} className - class name to be set
+	*/
+	function setBodyClass(className){
+		const body = document.body
+		body.className = className
+	}
+
+	/**
+	 * Changes theme toggle button's icon
+	 * @param {"sun" | "moon"} iconType - icon type to be displayed
+	*/
+	function changeBtnIcon(iconType){
+		const sunIcon = document.getElementById("sun-icon")
+		const moonIcon = document.getElementById("moon-icon")
+		if (iconType == "sun"){
+			moonIcon?.classList.remove("active-icon")
+			sunIcon?.classList.add("active-icon")
+		}
+		if (iconType == "moon"){
+			sunIcon?.classList.remove("active-icon")
+			moonIcon?.classList.add("active-icon")
+		}
+	}
